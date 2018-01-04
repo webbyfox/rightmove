4761e51d268a87a309af68b1cec27f8996e9ff8e
Rizwan Mansuri <=rmansuri@rcseng.ac.uk>
2018-01-04 11:22:55 +0000

    corrected typo

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ class CSVTest(unittest.TestCase):
         Setup for unit test
         """
         self.data = 'property-data.csv'
-        self.propreties = ParseCSV(self.data)
+        self.properties = ParseCSV(self.data)
 
     def test_csv_read_data_headers(self):
         """
         Test for CSV to have all required columns
         """
         self.assertEqual(
-            self.propreties.read_data()[0],
+            self.properties.read_data()[0],
             ['PROPERTY_REFERENCE', 'PRICE', 'BEDROOMS', 'BATHROOMS', 'HOUSE_NUMBER', 'ADDRESS', 'REGION', 'POSTCODE', 'PROPERTY_TYPE']
             )
 
@@ -24,19 +24,19 @@ class CSVTest(unittest.TestCase):
         """
         Test for for column data
         """
-        self.assertEqual(self.propreties.read_data()[1][1], '1000000')
+        self.assertEqual(self.properties.read_data()[1][1], '1000000')
 
     def test_csv_read_data_property_postcode(self):
         """
         Test for postcode column check
         """
-        self.assertEqual(self.propreties.read_data()[1][7], 'W1F 3FT')
+        self.assertEqual(self.properties.read_data()[1][7], 'W1F 3FT')
 
     def test_property_mean_value_for_postcode_outward(self):
         """
         Test for meanvalue check
         """
-        mean_value = self.propreties.get_mean_value_for_postcode_outward(self.propreties.read_data(), 'W1F')
+        mean_value = self.properties.get_mean_value_for_postcode_outward(self.properties.read_data(), 'W1F')
         self.assertEqual(mean_value, 1158750)
 
     def test_for_top_ten_most_expensive_properties(self):
@@ -44,7 +44,7 @@ class CSVTest(unittest.TestCase):
         Top ten percent of expansive property
         :return: 
         """
-        properties = self.propreties.top_ten_expensive_properties(self.propreties.read_data())
+        properties = self.properties.top_ten_expensive_properties(self.properties.read_data())
         self.assertEqual(len(properties), 2)
 
 
@@ -53,7 +53,7 @@ class CSVTest(unittest.TestCase):
         Test for average price diff between house and flat
         :return:
         """
-        avg_price = self.propreties.average_between_house_vs_flat(self.propreties.read_data())
+        avg_price = self.properties.average_between_house_vs_flat(self.properties.read_data())
         self.assertEqual(isinstance(avg_price, float), 1)
                           
 
