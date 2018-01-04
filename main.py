338da9b6b3d50108a1991363fb5d96fbac6b671f
Rizwan Mansuri <=rmansuri@rcseng.ac.uk>
2018-01-04 11:05:15 +0000

    Added further method for task and unittest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import statistics
 
 
 class ParseCSV(object):
@@ -7,18 +8,54 @@ class ParseCSV(object):
         self.data = data
 
     def read_data(self):
+        """
+        Parsing CSV file
+        :return: list of csv data
+        """
         with open(self.data, 'r') as f:
             parsed_data = [row for row in csv.reader(f.read().splitlines())]
         return parsed_data
 
-    
     def get_mean_value_for_postcode_outward(self, parsed_data, postcode_outward):
+        """
+        Return mean value of given postcode outward
+        :param parsed_data: list
+        :param postcode_outward: string
+        :return: float
+        """
         parsed_data.pop(0)
-        prices = [x[1] for x in parsed_data if x[7].startswith(postcode_outward)]
-        return prices
+        prices = [float(x[1]) for x in parsed_data if x[7].lower().startswith(postcode_outward.lower())]
+        return statistics.mean(prices)
+
+    def average_between_house_vs_flat(self, parsed_data):
+        """
+        Return average value between house vs flat
+        :param parsed_data: list
+        :return: float
+        """
+        parsed_data.pop(0)
+        flat_prices = [int(x[1]) for x in parsed_data if 'Flat' in x[8]]
+        house_prices = [int(x[1]) for x in parsed_data if 'Flat' not in x[8]]
+        return statistics.mean([float(x) - float(y) for x, y in zip(flat_prices, house_prices)])
+
+    def top_ten_expensive_properties(self, parsed_data):
+        """
+        Return top ten percentage of expansive properties
+        :param parsed_data: list
+        :return: list
+        """
+        parsed_data.pop(0)
+        sorted_data = list(reversed(sorted(parsed_data, key=lambda x: int(x[1]))))
+        top_ten_percentage = sorted_data[:int(len(sorted_data)*.10)]
+        return top_ten_percentage
 
 
-    def sort_property_by_proce(self, parsed_data):
-        parsed_data.pop(0)
-        properties = parsed_data.sort(lambda x: x[1])
-        return properties        
+
+if __name__ == '__main__':
+   properties = ParseCSV('property-data.csv')
+   properties_data = properties.read_data()
+   print('-'*100)
+   print("Mean value of properties: {0}".format(properties.get_mean_value_for_postcode_outward(properties_data, 'W1F')))
+   print("Average diff value between house vs flat : {0}".format(properties.average_between_house_vs_flat(properties_data)))
+   print("Top 10 % of expansive properties: {0}".format(properties.top_ten_expensive_properties(properties_data)))
+   print('-'*100)
--- test.py
+++ test.py
@@ -5,37 +5,57 @@ from main import ParseCSV
 class CSVTest(unittest.TestCase):
 
     def setUp(self):
+        """
+        Setup for unit test
+        """
         self.data = 'property-data.csv'
-        self.parsed_data = [
-            ['PROPERTY_REFERENCE','PRICE','BEDROOMS','BATHROOMS','HOUSE_NUMBER','ADDRESS','REGION','POSTCODE','PROPERTY_TYPE'],
-            ['1','1000000','7','2','12','Richard Lane','London','W1F 3FT','Mansion'],
-            ['24','755000','5','5','122','Jeff Street','Surrey','GU13 9OB','Mansion'],
-        ]
         self.propreties = ParseCSV(self.data)
 
     def test_csv_read_data_headers(self):
+        """
+        Test for CSV to have all required columns
+        """
         self.assertEqual(
             self.propreties.read_data()[0],
-            ['PROPERTY_REFERENCE','PRICE','BEDROOMS','BATHROOMS','HOUSE_NUMBER','ADDRESS','REGION','POSTCODE','PROPERTY_TYPE']
+            ['PROPERTY_REFERENCE', 'PRICE', 'BEDROOMS', 'BATHROOMS', 'HOUSE_NUMBER', 'ADDRESS', 'REGION', 'POSTCODE', 'PROPERTY_TYPE']
             )
 
     def test_csv_read_data_property_price(self):
+        """
+        Test for for column data
+        """
         self.assertEqual(self.propreties.read_data()[1][1], '1000000')
 
     def test_csv_read_data_property_postcode(self):
+        """
+        Test for postcode column check
+        """
         self.assertEqual(self.propreties.read_data()[1][7], 'W1F 3FT')
 
     def test_property_mean_value_for_postcode_outward(self):
+        """
+        Test for meanvalue check
+        """
         mean_value = self.propreties.get_mean_value_for_postcode_outward(self.propreties.read_data(), 'W1F')
-        self.assertEqual(mean_value,2323)
+        self.assertEqual(mean_value, 1158750)
 
-    # def test_get_min_score_difference(self):
-    #     self.assertEqual(self.football.get_min_score_difference(self.parsed_data), 1)
+    def test_for_top_ten_most_expensive_properties(self):
+        """
+        Top ten percent of expansive property
+        :return: 
+        """
+        properties = self.propreties.top_ten_expensive_properties(self.propreties.read_data())
+        self.assertEqual(len(properties), 2)
 
-    # def test_get_team(self):
-    #     index_value = self.football.get_min_score_difference(self.parsed_data)
-    #     self.assertEqual(self.football.get_team(index_value, self.parsed_data), 'Liverpool')
 
+    def test_for_average_diff_between_house_vs_flat(self):
+        """
+        Test for average price diff between house and flat
+        :return:
+        """
+        avg_price = self.propreties.average_between_house_vs_flat(self.propreties.read_data())
+        self.assertEqual(isinstance(avg_price, float), 1)
+                          
 
 if __name__ == '__main__':
     unittest.main()
